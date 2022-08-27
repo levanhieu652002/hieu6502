2d10d746cee74050afc7a1befe57134f793ea77a
Your Name <you@example.com>
2022-08-27 22:51:35 +0700

    first commit

---
+++ LeVanHieu_2050531200143_BT_GD2/app/src/main/java/com/example/levanhieu_2050531200143_bt_gd2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.levanhieu_2050531200143_bt_gd2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
